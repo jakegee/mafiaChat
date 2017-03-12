3702c7125c2cec1cdc003bbb5b6c6bebf201dd70
fxm630 <fxm630@04126ebf-8da6-4d40-86e0-4583bfa63b03>
2017-03-12 13:52:54 +0000

    added server visibility

--- mafiaChat/src/GUIs/chatGame.java
+++ mafiaChat/src/GUIs/chatGame.java
@@ -194,7 +194,7 @@ public class chatGame {
 
 
 		//text areas to view all users messages
-		//JTextArea txtServerMess = new JTextArea();
+		JTextArea txtServerMess = new JTextArea();
 		txtServerMess.setToolTipText("Messages received from Server");
 		txtServerMess.setEditable(false);
 		txtServerMess.setLineWrap(true);
